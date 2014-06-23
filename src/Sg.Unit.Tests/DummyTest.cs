24d518637f50ef9565dbacd7e9eea0a8e373ff53
Iain Ballard <iain.ballard@gmail.com>
2014-06-23 13:47:10 +0100

    Skeleton

---
+++ src/Sg.Unit.Tests/DummyTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Sg.Unit.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DummyTest
+    {
+
+        [Test]
+        public void dummy_test()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/SnivellingGit/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace SnivellingGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
